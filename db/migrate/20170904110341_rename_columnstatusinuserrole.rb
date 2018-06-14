a6db1b76b1fa6815033da931701d873d304afce1
Ashish <ashishkckse@gmail.com>
2018-06-14 15:21:13 +0530

    change in migration

--- db/migrate/20170904110341_rename_columnstatusinuserrole.rb
+++ db/migrate/20170904110341_rename_columnstatusinuserrole.rb
@@ -1,5 +1,5 @@
 class RenameColumnstatusinuserrole < ActiveRecord::Migration[5.1]
   def change
-  	rename_column :users, :status, :role, default: 0
+  	rename_column :users, :status, :role
   end
 end
